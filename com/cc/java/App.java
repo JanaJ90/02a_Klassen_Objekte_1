eb78c58715a962372bc0fae26c7214e2954c97bf
Ja0n0aJ <jana.janzen1@web.de>
2024-07-12 12:50:03 +0200

    Java Klassen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,8 +2,16 @@ package com.cc.java;
 
 public class App {
     
-    public static void main(String[] args) {
+    public static void main(String[] args)
+     {
+       
+       //Instaziierung --> Erzeugen ein Objektes
+        // System.out.println(new Cat());
 
+        //kleine cat-->Referenzvariable
+        //große Cat-->Refernztyp(Klasse)
+      Cat cat = new Cat();
+      System.out.println(cat);
 
     }
 
